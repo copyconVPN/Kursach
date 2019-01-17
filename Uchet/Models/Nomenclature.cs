ea41890c08f5cc8d710ac97e2f53ce8189b3d423
Павел Вологжанин <vpn@copycon.ru>
2019-01-17 23:36:26 +0400

    555

--- Uchet/Models/Nomenclature.cs
+++ Uchet/Models/Nomenclature.cs
@@ -2,16 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Uchet.Models
 {
     public class Nomenclature
     {
+        [Key]
         public int Id { get; set; }
+
+        [Display(Name = "Наименование")]
+        [Required(ErrorMessage = "Наименование не указанно")]
         public string Name { get; set; }
+
+        [Display(Name = "Сорт")]
         public string Sort { get; set; }
+
+        [Display(Name = "Единицы измерения")]
+        [Required(ErrorMessage = "Единицы измерения не указаны")]
         public string Unit { get; set; }
+
+        [Display(Name = "Характеристика")]
         public string Property { get; set; }
+
+        [Display(Name = "Комментарий")]
         public string Comment { get; set; }
     }
 }
