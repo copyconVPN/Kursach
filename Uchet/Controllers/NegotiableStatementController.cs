c37355b8cc72bce8d0e0222c215640df2b05f7d5
Максим Шишков <stalker_nova@mail.ru>
2019-01-18 00:07:47 +0400

    t2

---
+++ Uchet/Controllers/NegotiableStatementController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Uchet.Controllers
+{
+    abstract class ReportDate
+    {
+        private string name;
+        private int age;
+        public int Age
+        {
+            set
+            {
+                if (value < 18)
+                {
+                    Console.WriteLine("Возраст должен быть больше 17");
+                }
+                else
+                {
+                    age = value;
+                }
+            }
+            get { return age; }
+        }
+
+        int Sum(int a, int b)
+        { return a + b; }
+
+        double Sum(double a, double b)
+        { return a + b; }
+    }
+
+    public class NegotiableStatementController : Controller
+    {
+        // GET: NegotiableStatement
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
